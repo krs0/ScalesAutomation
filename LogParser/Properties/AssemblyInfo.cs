ce016771299efb5408f740f06e44f2735ad8f07b
krs <cristianlaczko@yahoo.com>
2019-02-18 23:52:04 +0200

    Added automatic revisions

--- LogParser/Properties/AssemblyInfo.cs
+++ LogParser/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -33,5 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.19049.2150")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Log4Net.config", Watch = true)]
+
+[assembly: AssemblyInformationalVersion("1.0.0-rc-02")]
