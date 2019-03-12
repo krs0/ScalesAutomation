ad0162ee784aaeeafae07408360d007ae529de83
krs <cristianlaczko@yahoo.com>
2019-03-12 21:30:36 +0200

    Version 2.1 Released

--- LogParser/Properties/AssemblyInfo.cs
+++ LogParser/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.19064.2136")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.19064.2136")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Log4Net.config", Watch = true)]
 
-[assembly: AssemblyInformationalVersion("2.1.0-rc-01")]
+[assembly: AssemblyInformationalVersion("2.2.0")]
