b7f55c61d670a0ed2d66ea03d3ec5dca0b053b21
krs <krs0@users.noreply.github.com>
2023-01-24 14:25:03 +0200

    Added log4net to common

--- Common/Common.cs
+++ Common/Common.cs
@@ -1,7 +1,12 @@
-﻿namespace Common
+﻿using log4net;
+using System.Reflection;
+
+namespace Common
 {
     public class Common
     {
+        readonly static ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
 
     }
 }
