d66def90ba8ce477d9b2260f903a24fb18fece9c
krs <krs0@users.noreply.github.com>
2023-01-26 00:10:54 +0200

    Excel part of metrology Reader extracted in separate class.

---
+++ MetrologyReader/Metrology.cs
@@ -0,0 +1,61 @@
+﻿using log4net;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MetrologyReader
+{
+    public class Metrology
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        Excel.Application excelApp;
+        Excel.Workbook workbook;
+        Excel.Sheets worksheets;
+        Excel.Worksheet worksheet;
+
+        public void GetMetrologyResult(string measurementsFileName)
+        {
+            excelApp.EnableEvents = true; // we need to manually enable here because by default Excel events are disabled
+
+            // set the measurements file name in a special cell that is monitored for changed even within Excel (this event will select this file in files dropdown and run all macros).
+            worksheet.Range["MeasurementsFileName"].Value2 = measurementsFileName;
+
+            excelApp.EnableEvents = false; // disable Excel events again
+
+            log.Debug($"Written value in MeasurementsFileName Range: {worksheet.Range["MeasurementsFileName"].Text.ToString()}");
+
+            string? measurementsOverallStatus = worksheet.Range["A2"].Text.ToString();
+            log.Info($"Measurement overall status: {measurementsOverallStatus}");
+
+            // write in console so that calling process can catch this result
+            Console.WriteLine(measurementsOverallStatus);
+        }
+
+        public void InitializeExcel(string centralizatorFilePath)
+        {
+            // Open Excel in non-visible mode
+            excelApp = new Excel.Application
+            {
+                Visible = false,
+                DisplayAlerts = false
+            };
+            workbook = excelApp.Workbooks.Open(centralizatorFilePath);
+            worksheets = (Excel.Sheets)workbook.Worksheets;
+            worksheet = (Excel.Worksheet)workbook.Worksheets["Metrologie"];
+        }
+
+        public void CloseExcel()
+        {
+            // Close the workbook and Excel application
+            workbook.Close(true);
+            excelApp.Quit();
+
+            // Release the Excel objects
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheets);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+        }
+
+    }
+}
--- MetrologyReader/MetrologyReader.cs
+++ MetrologyReader/MetrologyReader.cs
@@ -1,65 +1,53 @@
 using log4net;
 using System.Reflection;
-using Excel = Microsoft.Office.Interop.Excel;
 
-class MetrologyReader
+namespace MetrologyReader
 {
-    private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
-    static void Main(string[] args)
+    class MetrologyReader
     {
-        if(args.Length < 2)
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
         {
-            log.Error($"Please provide the filepath for centralizator masuratori and the desired measurements file name to be interrogated as arguments.");
-            return;
+            string centralizatorFilePath, measurementsFileName;
+
+            try
+            {
+                ParseArgs(args, out centralizatorFilePath, out measurementsFileName);
+
+                Metrology metrologyReader = new Metrology();
+
+                metrologyReader.InitializeExcel(centralizatorFilePath);
+
+                metrologyReader.GetMetrologyResult(measurementsFileName);
+
+                metrologyReader.CloseExcel();
+            }
+            catch(Exception ex)
+            {
+                log.Error($"Error: {ex.Message}");
+            }
         }
 
-        string centralizatorFilePath = args[0];
-        string measurementsFileName = args[1];
-        log.Info($"Metrology Reader Started with the following arguments:{System.Environment.NewLine}" +
-            $"Centralizator Masuratori File Path = {centralizatorFilePath}{System.Environment.NewLine}" +
-            $"Measurements File Name = {measurementsFileName}{System.Environment.NewLine}");
-
-        if(!File.Exists(centralizatorFilePath))
+        private static void ParseArgs(string[] args, out string centralizatorFilePath, out string measurementsFileName)
         {
-            log.Error($"Centralizator File does not exist!");
-            return;
+            if(args.Length < 2)
+            {
+                log.Error($"Please provide the filepath for centralizator masuratori and the desired measurements file name to be interrogated as arguments.");
+                throw new Exception();
+            }
+
+            centralizatorFilePath = args[0];
+            measurementsFileName = args[1];
+            log.Info($"Metrology Reader Started with the following arguments:{System.Environment.NewLine}" +
+                $"Centralizator Masuratori File Path = {centralizatorFilePath}{System.Environment.NewLine}" +
+                $"Measurements File Name = {measurementsFileName}{System.Environment.NewLine}");
+
+            if(!File.Exists(centralizatorFilePath))
+            {
+                log.Error($"Centralizator File does not exist!");
+                throw new Exception();
+            }
         }
-
-        // Open Excel in non-visible mode
-        Excel.Application excelApp = new Excel.Application
-        {
-            Visible = false,
-            DisplayAlerts = false
-        };
-
-        Excel.Workbook workbook = excelApp.Workbooks.Open(centralizatorFilePath);
-        Excel.Sheets worksheets = (Excel.Sheets)workbook.Worksheets;
-        Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets["Metrologie"];
-
-        excelApp.EnableEvents = true; // we need to manually enable here because by default Excel events are disabled
-
-        // set the measurements file name in a special cell that is monitored for changed even within Excel (this event will select this file in files dropdown and run all macros).
-        worksheet.Range["MeasurementsFileName"].Value2 = measurementsFileName;
-
-        excelApp.EnableEvents = false; // disable Excel events again
-
-        log.Debug($"Written value in MeasurementsFileName Range: {worksheet.Range["MeasurementsFileName"].Text.ToString()}");
-
-        string? measurementsOverallStatus = worksheet.Range["A2"].Text.ToString();
-        log.Info($"Measurement overall status: {measurementsOverallStatus}");
-
-        // write in console so that calling process can catch this result
-        Console.WriteLine(measurementsOverallStatus);
-
-        // Close the workbook and Excel application
-        workbook.Close(true);
-        excelApp.Quit();
-
-        // Release the Excel objects
-        System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
-        System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheets);
-        System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
-        System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
     }
 }
