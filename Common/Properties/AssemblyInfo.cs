851a252e4a9db5e4316540aabecb0f5d8378a5a3
krs <cristianlaczko@yahoo.com>
2023-02-05 21:19:27 +0200

    Increased version numbers for all aps to 2.6.0

--- Common/Properties/AssemblyInfo.cs
+++ Common/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.3.8430")]
-[assembly: AssemblyFileVersion("2.5.3.8430")]
-[assembly: AssemblyInformationalVersion("2.4.3")]
+[assembly: AssemblyVersion("2.6.0.8430")]
+[assembly: AssemblyFileVersion("2.6.0.8430")]
+[assembly: AssemblyInformationalVersion("2.6.0")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Log4Net.config", Watch = true)]
