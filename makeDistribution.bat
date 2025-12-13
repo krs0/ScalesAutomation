d3ce16d139f91d765a0d85fd02b843863d26a7ab
krs <cristianlaczko@yahoo.com>
2025-12-13 22:14:41 +0200

    Moved /out at same level with /src

--- makeDistribution.bat
+++ makeDistribution.bat
@@ -1,4 +1,4 @@
-robocopy *.* .\src\out\Release\ .\distribution\ScalesAutomation\
+robocopy *.* .\out\Release\ .\distribution\ScalesAutomation\
 robocopy *.* .\src\resources\Images\ .\distribution\ScalesAutomation\Images\
 robocopy *.* .\resources .\distribution\Server CentralizatorMasuratori.xlsm /XF "Manual - Automatizare Cantar.docx"
 robocopy .\resources .\distribution\ScalesAutomation CentralizatorMasuratori.xlsm
