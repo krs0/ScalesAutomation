0871df82c8fc11e6b8ef265beb5670b18b5b408f
krs <cristianlaczko@yahoo.com>
2025-12-14 13:07:31 +0200

    Moved scripts in /scripts folder

--- makeDistribution.bat
+++
@@ -1,4 +0,0 @@
-robocopy *.* .\out\Release\ .\distribution\ScalesAutomation\
-robocopy *.* .\src\resources\Images\ .\distribution\ScalesAutomation\Images\
-robocopy *.* .\resources .\distribution\Server CentralizatorMasuratori.xlsm /XF "Manual - Automatizare Cantar.docx"
-robocopy .\resources .\distribution\ScalesAutomation CentralizatorMasuratori.xlsm
---
+++ scripts/makeDistribution.bat
@@ -0,0 +1,4 @@
+robocopy *.* .\..\out\Release\ .\..\distribution\ScalesAutomation\
+robocopy *.* .\..\src\resources\Images\ .\..\distribution\ScalesAutomation\Images\
+robocopy *.* .\..\resources .\..\distribution\Server CentralizatorMasuratori.xlsm /XF "Manual - Automatizare Cantar.docx"
+robocopy .\..\resources .\..\distribution\ScalesAutomation CentralizatorMasuratori.xlsm
